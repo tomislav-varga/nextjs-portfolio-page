8050dc86edd2a3372c046d18d6486f12855fadf8
tomi <tomislav.varga@icloud.com>
2025-07-05 08:40:53 +0200

    update: margin in p elements

--- src/components/sections/about-section.tsx
+++ src/components/sections/about-section.tsx
@@ -9,11 +9,11 @@ export function AboutSection() {
             Mit Expertise in Frontend- und Backend-Technologien strebe ich danach, skalierbare und
             wartbare Lösungen zu entwickeln, die großartige Benutzererfahrungen bieten.
           </p>
-          <p className="text-lg text-muted-foreground">
+          <p className="text-lg text-muted-foreground mb-6">
             Wenn ich nicht programmiere, kann man mich dabei finden, neue Technologien zu erkunden, zu Open-Source-Projekten
             beizutragen oder mein Wissen mit der Entwickler-Community zu teilen.
           </p>
-          <p className="text-lg text-muted-foreground max-w-[600px]">
+          <p className="text-lg text-muted-foreground mb-6">
             Klicke <a href="/about" className="font-bold">hier</a>, um mehr über mich zu erfahren.
           </p>
         </div>
